873f6de46983782b5d1ed8f61d7b318e9455c94d
Odegov <iavode@mail.ru>
2020-12-18 16:24:10 +0300

    Add: script organize imports.

---
+++ __init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 12:10:52 2020
+
+@author: iavode
+"""
+import models
+
+import parsers
+from .parsers import (
+    otvetmailparser, ekatalogparser, qaloader, reviewloader
+)
+from .parsers import (
+    get_question_answers, get_product_reviews, get_products_reviwes,
+    questions_answers_loader
+)
+
+import utils
+from .utils import (
+    count_parameters, create_model, create_tokenizer, DataLoader, get_page,
+    log, preprocessing_result, smart_batching, Embedder
+)
